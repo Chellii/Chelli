eb3ca1e7fb6a91a0abb1962ce2c5fe69c3976102
Chaimae El oiriagli <56842722+Chellii@users.noreply.github.com>
2022-06-29 18:59:00 +0100

    menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,16 +2,32 @@ import "../styles/header.css"
 import {GiMoonBats} from "react-icons/gi"
 import { Link } from 'react-router-dom';
 import { makeStyles } from "@mui/styles"
+import { useState } from "react";
 
 const useStyles = makeStyles({
+    headeroot: {
+        padding: 0,
+        fontFamily: "'Henny Penny', cursive",
+        fontSize: 40,
+        color: 'black',
+        ['@media only screen and (max-width: 600px)'] : {
+            fontSize: 50,
+          },
+      '&:link': {
+          textDecoration: 'none'
+      },
+    },
     root: {
         padding: 0,
         margin: '0 15 0 15',
         fontFamily: "'Henny Penny', cursive",
         fontSize: 25,
         color: '#33272A',
-        ['@media only screen and (max-width: 455px)'] : {
-            fontSize: 35,
+        ['@media only screen and (max-width: 600px)'] : {
+            fontSize: 80,
+            '&:hover' : {
+                backgroundColor: 'black'
+            }
           },
       '&:link': {
           textDecoration: 'none'
@@ -23,16 +39,23 @@ const useStyles = makeStyles({
   });
 
 export default function Header() {
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => setIsClicked(!isClicked);
     const classes = useStyles();
     return (
         <header className="header--container">
-            <Link className={classes.root} to="/"><h1>Chelli</h1></Link>
-            <ul className="header-ul">
-                <li><Link className={classes.root} to="/about">About</Link></li>
-                <li><Link className={classes.root} to="projects">Projects</Link></li>
-                <li><Link className={classes.root} to="articles">Articles</Link></li>
-                <li ><GiMoonBats size={40}  /></li>
-            </ul>
+            <Link className={classes.headeroot} to="/"><h1>Chelli</h1></Link>
+                <ul className={isClicked ? "header-ul clicked" : "header-ul"}>
+                    <li onClick={handleClick}><Link className={classes.root} to="/about">About</Link></li>
+                    <li onClick={handleClick}><Link className={classes.root} to="projects">Projects</Link></li>
+                    <li onClick={handleClick}><Link className={classes.root} to="articles">Articles</Link></li>
+                    <li onClick={handleClick}><GiMoonBats className={classes.moon} size={50}  /></li>
+                </ul>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={isClicked ? 'fas fa-times' : 'fas fa-bars'}/>
+                </div>
         </header>
     )
 }
--- src/Components/LatestProject.js
+++ src/Components/LatestProject.js
@@ -8,7 +8,7 @@ const useStyles = makeStyles({
       fontSize: 18,
       margin: 0,
       color:"#4e4d4d !important",
-      ['@media only screen and (max-width: 455px)'] : {
+      ['@media only screen and (max-width: 600px)'] : {
         fontSize: 35,
         color: '#a26700ef !important'
       },
