c143a9fb93f2b4a260e024e31efe9acc6a16b94a
chelli <56842722+Chellii@users.noreply.github.com>
2022-07-20 22:47:59 +0100

    adding data

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,18 @@
 export default [
     {
         id: 0,
+        title: 'Github Api Project',
+        description: 'I created a project using Github Api, where you can search user by its username and it will give you the users informations and followers.',
+        link: 'https://github-mockapi.herokuapp.com/'
+    },
+    {
+        id: 1,
         title: 'Password Generator',
         description: 'For lazy people who loves to have strong password. this website generator secure passwords that are impossible to crack.',
         link: 'https://chellii.github.io/Password-Generator/'
     },
     {
-        id: 1,
+        id: 2,
         title: 'Je suis Marocaine Je suis Digital',
         description: "Being a woman in tech means building a strong network of supporters. I built with the help of staff 1337 khouribga a website where we motivate women to enter the world of IT.",
         link: "https://github.com/Chellii/jesuismarocainejesuisdigital"
@@ -18,19 +24,19 @@ export default [
         link: "https://chellii.github.io/Game_Repo/snake_game/"
     },
     {
-        id: 2,
+        id: 4,
         title: 'Travel Journal',
         description: 'Website where you can document your travels and experiences. Created with React js.',
         link: 'https://github.com/Chellii/Travel-Journal'
     },
     {
-        id: 4,
+        id: 5,
         title: 'Fractol',
         description: "Fractol is a 42 school Project aimed at exploring fractals; mathematical sets that exhibit a repeating pattern displayed at every scale.",
         link: "https://github.com/Chellii/Fractol-42"
     },
     {
-        id: 5,
+        id: 6,
         title: 'RT',
         description: "For graphic and Math lovers. this is a 42 school project, we have to create from scratch 2D object and make them look like 3D objects using language C and Raytracing",
         link: "https://github.com/Chellii/RT-42"
