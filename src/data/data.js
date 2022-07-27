81064934c18d61137afd404ae129925726cd3b9f
chelli <56842722+Chellii@users.noreply.github.com>
2022-07-27 21:02:22 +0100

    modification of data

--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,9 @@
 export default [
     {
         id: 0,
-        title: 'Github Api Project',
-        description: 'I created a project using Github Api, where you can search user by its username and it will give you the users informations and followers.',
-        link: 'https://github-mockapi.herokuapp.com/'
+        title: 'Clear Path Canada Project',
+        description: 'Currently building this Website with another person for a company that helps people to move to Canada',
+        link: 'https://clearpathcanada.ca/'
     },
     {
         id: 1,
@@ -19,24 +19,30 @@ export default [
     },
     {
         id: 3,
+        title: 'Github Api Project',
+        description: 'I created a project using Github Api, where you can search user by its username and it will give you the users informations and followers.',
+        link: 'https://github-mockapi.herokuapp.com/'
+    },
+    {
+        id: 4,
         title: "Snake Game",
         description: "Old snake game, still in progress but worth to put it here",
         link: "https://chellii.github.io/Game_Repo/snake_game/"
     },
     {
-        id: 4,
+        id: 5,
         title: 'Travel Journal',
         description: 'Website where you can document your travels and experiences. Created with React js.',
         link: 'https://github.com/Chellii/Travel-Journal'
     },
     {
-        id: 5,
+        id: 6,
         title: 'Fractol',
         description: "Fractol is a 42 school Project aimed at exploring fractals; mathematical sets that exhibit a repeating pattern displayed at every scale.",
         link: "https://github.com/Chellii/Fractol-42"
     },
     {
-        id: 6,
+        id: 7,
         title: 'RT',
         description: "For graphic and Math lovers. this is a 42 school project, we have to create from scratch 2D object and make them look like 3D objects using language C and Raytracing",
         link: "https://github.com/Chellii/RT-42"
