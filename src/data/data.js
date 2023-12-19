09b8790df68aee0b41785138281efe2f91981d69
Chaimae El Oiriagli <56842722+Chellii@users.noreply.github.com>
2023-12-19 21:47:02 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,54 +1,60 @@
 export default [
     {
         id: 0,
+        title: 'Official Moroccan Railways Ticketing Platform',
+        description: "Using React for the site's look and feel and Node.js for its functionality behind the scenes, I crafted a user-friendly platform. This website ensures an easy, responsive, and secure ticket booking experience for travelers.",
+        link: "https://www.oncf-voyages.ma/"
+    },
+    {
+        id: 1,
         title: 'Clear Path Canada Project',
         description: 'Built this Website with another person for a company that helps people to move to Canada',
         link: 'https://clearpathcanada.ca/'
     },
     {
-        id: 1,
+        id: 2,
         title: 'Password Generator',
         description: 'For lazy people who loves to have strong password. this website generator secure passwords that are impossible to crack.',
         link: 'https://chellii.github.io/Password-Generator/'
     },
     {
-        id: 2,
+        id: 3,
         title: 'Je suis Marocaine Je suis Digital',
         description: "Being a woman in tech means building a strong network of supporters. I built with the help of staff 1337 khouribga a website where we motivate women to enter the world of IT.",
         link: "https://github.com/Chellii/jesuismarocainejesuisdigital"
     },
     {
-        id: 3,
+        id: 4,
         title: 'Unsplash Gallery',
         description: "This is website that use lazy loading to show a gallery of images but before entering the gallery you should sign in.",
         link: "https://github.com/Chellii/jesuismarocainejesuisdigital"
     },
     {
-        id: 4,
+        id: 5,
         title: 'Github Api Project',
         description: 'I created a project using Github Api, where you can search user by its username and it will give you the users informations and followers.',
         link: 'https://github-mockapi.herokuapp.com/'
     },
     {
-        id: 5,
+        id: 6,
         title: "Snake Game",
         description: "Old snake game, still in progress but worth to put it here",
         link: "https://chellii.github.io/Game_Repo/snake_game/"
     },
     {
-        id: 6,
+        id: 7,
         title: 'Travel Journal',
         description: 'Website where you can document your travels and experiences. Created with React js.',
         link: 'https://github.com/Chellii/Travel-Journal'
     },
     {
-        id: 6,
+        id: 8,
         title: 'Fractol',
         description: "Fractol is a 42 school Project aimed at exploring fractals; mathematical sets that exhibit a repeating pattern displayed at every scale.",
         link: "https://github.com/Chellii/Fractol-42"
     },
     {
-        id: 7,
+        id: 9,
         title: 'RT',
         description: "For graphic and Math lovers. this is a 42 school project, we have to create from scratch 2D object and make them look like 3D objects using language C and Raytracing",
         link: "https://github.com/Chellii/RT-42"
